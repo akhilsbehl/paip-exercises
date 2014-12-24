0b3923c9397fa69a388539eddf80419b87327106
akhilsbehl <akhilsbehl@gmail.com>
2014-12-25 02:05:37 +0530

    Added a solution to exercise 2.4.

--- src/chapter-2.lisp
+++ src/chapter-2.lisp
@@ -1,7 +1,5 @@
 (defun cross-product (func alist blist)
-  "Exercise 2.4: Cross-product of a list of argument lists. Combine all is
-  simply this function itself since it is a cross-product on the function
-  append."
+  "Exercise 2.4: Cross-product of two lists over a given function."
   (cond
     ((or (null alist) (null blist)) nil)
     ((not (equalp (length alist) (length blist))) nil)
