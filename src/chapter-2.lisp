597d6713a4b429b9a5f4fe4580c7d3a1626ccf9c
akhilsbehl <akhilsbehl@gmail.com>
2014-12-25 02:04:33 +0530

    Added a solution to exercise 2.4.

--- src/chapter-2.lisp
+++ src/chapter-2.lisp
@@ -1,12 +1,9 @@
-(defun cross-two (alist blist)
-  (let ((retval nil))
-    (dolist (a alist (nreverse retval))
-      (dolist (b blist)
-        (push (cons a (if (listp b) b (list b))) retval)))))
-
-(defun cross-product (arg-lists combiner)
-  "Exercise 2.4: Cross-product of a list of argument lists."
+(defun cross-product (func alist blist)
+  "Exercise 2.4: Cross-product of a list of argument lists. Combine all is
+  simply this function itself since it is a cross-product on the function
+  append."
   (cond
-    ((null arg-lists) nil)
-    ((equalp 1 (length arg-lists)) (first arg-lists))
-    (t (reduce #'cross-two arg-lists :from-end t))))
+    ((or (null alist) (null blist)) nil)
+    ((not (equalp (length alist) (length blist))) nil)
+    (t (mapcan (lambda (x)
+                 (mapcar (lambda (y) (funcall func x y)) alist)) blist))))
