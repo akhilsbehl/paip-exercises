429aa3859b82bcccdb9257f201587bc8cfd1d49a
akhilsbehl <akhilsbehl@gmail.com>
2014-12-25 03:30:42 +0530

    Added another solution to exercise 2.4.

--- src/chapter-2.lisp
+++ src/chapter-2.lisp
@@ -3,5 +3,16 @@
   (cond
     ((or (null alist) (null blist)) nil)
     ((not (equalp (length alist) (length blist))) nil)
-    (t (mapcan (lambda (x)
-                 (mapcar (lambda (y) (funcall func x y)) alist)) blist))))
+    (t (mapcan (lambda (b)
+                 (mapcar (lambda (a) (funcall func a b)) alist)) blist))))
+
+(defun do-cross-product (func alist blist)
+  "Exercise 2.4: Cross-product of two lists over a given function.
+  A different approach based on iteration. Both are functionally identical."
+  (cond
+    ((or (null alist) (null blist)) nil)
+    ((not (equalp (length alist) (length blist))) nil)
+    (t (let ((result nil))
+         (dolist (b blist (nreverse result))
+           (dolist (a alist)
+             (push (funcall func a b) result)))))))
