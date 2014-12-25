3ef325f0e1bd97bb8cffe714cb04f9985f269266
akhilsbehl <akhilsbehl@gmail.com>
2014-12-25 15:30:33 +0530

    Added the solution to exercise 3.2.

--- src/chapter-3.lisp
+++ src/chapter-3.lisp
@@ -1,2 +1,24 @@
 ;;;; Exercise 3.1. Now why would I need multiple lambdas?
 ((lambda (x) (+ x (* x x))) 6)
+
+;;;; Exercise 3.2. Hmm... dunno... `reverse` + `append`?
+
+(defun print-dotted (expr)
+  "Exercise 3.3: Print an arbitrary expression in the dotted pair notation. I
+  understand this to be: print the cons structure of the expression list."
+  (cond ((null expr) (princ nil))
+        ((atom expr) (princ expr))
+        (t (progn
+             (princ "(")
+             (print-dotted (car expr))
+             (princ " . ")
+             (print-dotted (cdr expr))
+             (princ ")"))))
+  nil)
+
+(print-dotted nil)
+(print-dotted 'a)
+(print-dotted '(a b c))
+(print-dotted '(a b (c d) e))
+
+()
